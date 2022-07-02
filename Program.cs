5c6eaa29a8c617d00c7eee60a045ae636a19a261
«Egor» <s_megorm_s@mail.ru>
2022-07-02 18:25:41 +0300

    the first C# lesson

---
+++ Program.cs
@@ -0,0 +1,135 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+int n1 = 49;
+int n2 = 7;
+if (n1 == n2 * n2)
+{
+    Console.WriteLine("n1 - квадрат числа n2");
+}
+else
+{
+    Console.WriteLine("n1 - не квадрат числа n2");
+}
+*/
+
+/*
+//задача 3
+Console.WriteLine("Введите число:");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (day == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (day == 3)
+{
+    Console.WriteLine("Среда");
+}
+else 
+{
+    Console.WriteLine("День недели введен неверно");
+}
+*/
+
+/*
+//Задача 5
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int start = -Math.Abs(n);
+int finish = Math.Abs(n);
+
+Console.WriteLine("Начало ввода: ");
+while (start <= finish)
+{
+    Console.WriteLine(start);
+    start++;
+}
+*/
+
+/*
+//задача 7
+
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+int b;
+b = n%10;
+Console.WriteLine("Последнее число");
+Console.WriteLine(b);
+*/
+
+//Домашнее задание
+
+/*
+//Задание 1
+
+int n1 = 15;
+int n2 = 24;
+
+if (n1 > n2)
+{
+    Console.WriteLine("max = 15");
+}
+else
+{
+    Console.WriteLine("max = 24");
+}
+*/
+
+/*
+//Задание 2
+
+Console.WriteLine("Введите число:");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число:");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число:");
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 > n2 && n1 > n3)
+{
+    Console.WriteLine("max = " +n1);
+}
+else if (n2 > n1 && n2 > n3)
+{
+    Console.WriteLine("max = " +n2);
+}
+else 
+{
+    Console.WriteLine("max = " +n3);
+}
+*/
+
+/*
+//Задание 3
+
+Console.WriteLine("Введите число:");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 % 2==0)
+{
+    Console.WriteLine("число - четное");
+}
+else 
+{
+    Console.WriteLine("число - нечетное");
+}
+*/
+
+
+//Задание 4
+/*
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int a = 2;
+while (a < n)
+{
+    Console.WriteLine(a);
+    a += 2;
+}
+*/
